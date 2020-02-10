ac6d8c9c87fed96aa8daa32ff66bdd300bbd7e35
rickybag <rickybag99@gmail.com>
2020-02-11 00:34:44 +0100

    removed fontawesome css

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
 
-import "font-awesome/css/font-awesome.min.css";
-
 import "./assets/scss/styles.scss";
 
 ReactDOM.render(
