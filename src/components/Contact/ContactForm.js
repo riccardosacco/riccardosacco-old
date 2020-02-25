1e0dd50303b550f9da4b3e7af49404ae51f2e3e2
rickybag <rickybag99@gmail.com>
2020-02-25 18:26:29 +0100

    added netlify form

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -1,28 +1,45 @@
 import React from "react";
 
+import NetlifyForm from "react-netlify-form";
+
 const ContactForm = () => {
   return (
-    <form action="" className="contact-form" id="contact-form">
-      <input
-        className="contact-input"
-        placeholder="Name"
-        type="text"
-        required
-      />
-      <input
-        className="contact-input"
-        placeholder="Email"
-        type="email"
-        required
-      />
-      <textarea
-        className="contact-input"
-        placeholder="Message"
-        id=""
-        rows="10"
-      ></textarea>
-      <div className="button button-block">Contact Me</div>
-    </form>
+    <NetlifyForm name="Contact Form">
+      {({ loading, error, success }) => (
+        <div className="contact-form" id="contact-form">
+          {loading && <div className="alert alert-info">Loading...</div>}
+          {error && (
+            <div className="alert alert-error">
+              Your information was not sent. Please try again later.
+            </div>
+          )}
+          {success && (
+            <div className="alert alert-success">
+              Thank you for contacting me!
+            </div>
+          )}
+          <input
+            className="contact-input"
+            placeholder="Name"
+            type="text"
+            required
+          />
+          <input
+            className="contact-input"
+            placeholder="Email"
+            type="email"
+            required
+          />
+          <textarea
+            className="contact-input"
+            placeholder="Message"
+            id=""
+            rows="10"
+          ></textarea>
+          <div className="button button-block">Contact Me</div>
+        </div>
+      )}
+    </NetlifyForm>
   );
 };
 
