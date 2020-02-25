d48299f531378070e37111a7beef9c39ff004879
rickybag <rickybag99@gmail.com>
2020-02-25 19:28:11 +0100

    finished contact form

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -7,7 +7,12 @@ const encode = data => {
 };
 
 const ContactForm = () => {
-  const [form, setForm] = useState({});
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: ""
+  });
+  const [success, setSuccess] = useState(false);
 
   const handleChange = e => {
     setForm({
@@ -25,16 +30,30 @@ const ContactForm = () => {
       body: encode({ "form-name": "contact", ...form })
     });
 
-    console.log(response);
+    setSuccess(response.status === 200 ? true : false);
+    setForm({
+      name: "",
+      email: "",
+      message: ""
+    });
   };
 
   return (
     <form className="contact-form" onSubmit={handleSubmit}>
+      {success && (
+        <div className="alert alert-success alert-custom">
+          Thank your for your contact request! I'll email you soon!
+          <div className="close-icon" onClick={() => setSuccess(false)}>
+            <i className="fas fa-times"></i>
+          </div>
+        </div>
+      )}
       <input
         id="name"
         className="contact-input"
         placeholder="Name"
         type="text"
+        value={form.name}
         onChange={handleChange}
         required
       />
@@ -43,6 +62,7 @@ const ContactForm = () => {
         className="contact-input"
         placeholder="Email"
         type="email"
+        value={form.email}
         onChange={handleChange}
         required
       />
@@ -50,6 +70,7 @@ const ContactForm = () => {
         id="message"
         className="contact-input"
         placeholder="Message"
+        value={form.message}
         onChange={handleChange}
         rows="10"
       ></textarea>
