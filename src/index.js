0c1c1618ce6cc9e4b4312636032056cac53a325c
rickybag <rickybag99@gmail.com>
2020-02-25 20:52:55 +0100

    added google analytics

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import ReactGA from "react-ga";
 
 import ScrollToTop from "./routing/ScrollToTop";
 
 import App from "./App";
 
 import "./assets/scss/styles.scss";
+
+// Initialize Google Analytics
+ReactGA.initialize("UA-159074991-1");
 
 ReactDOM.render(
   <Router>
